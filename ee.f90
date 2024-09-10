6aae241bce9e9e47172dba287ed224054b02fbf3
JiPang22 <wlvud6902@gmail.com>
2024-09-10 16:06:25 +0900

    aa

--- ee.f90
+++ ee.f90
@@ -1,14 +1,17 @@
 program aa
 implicit none
-integer i,imax, num_peaks
+integer i,imax, num_peaks, peak_search_interval
 parameter(imax=10000)
 real t,dt,x,dx,y,dy,gam, avg_log_decrement, damping_ratio
 real, dimension(imax) :: xt
 real, dimension(imax) :: peaks
 integer, dimension(imax) :: peak_indices
 
+! 피크 탐색 간격 설정 (사용자가 원하는 값으로 설정)
+peak_search_interval = 2  ! 피크 탐색 간격 (예: 2로 설정하여 두 포인트마다 탐색)
+
 ! 시스템 조건 설정
-dt = 1.e-3
+dt = 2.e-3
 gam = 0.1
 
 ! 초기 조건 설정
@@ -28,9 +31,9 @@ y = y + dy*dt
 t = i*dt
 enddo
 
-! 피크 찾기
+! 피크 찾기 (간격 조정 가능)
 num_peaks = 0
-do i = 2, imax-1
+do i = 2, imax-1, peak_search_interval
 if (xt(i) > xt(i-1) .and. xt(i) > xt(i+1)) then
 num_peaks = num_peaks + 1
 peaks(num_peaks) = xt(i)
@@ -47,7 +50,7 @@ end do
 avg_log_decrement = avg_log_decrement / (num_peaks - 1)
 
 ! 감쇠비 계산
-damping_ratio = avg_log_decrement / sqrt(4.0 * 3.14159265**2 + avg_log_decrement**2)
+damping_ratio = avg_log_decrement / (2.0 * 3.14159265)
 
 print *, '평균 대수 감쇠율: ', avg_log_decrement
 print *, '감쇠비: ', damping_ratio
