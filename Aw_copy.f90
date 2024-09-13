1847923a24927d23cca00592bb5d8a280efff4c9
JiPang22 <wlvud6902@gmail.com>
2024-09-13 17:56:27 +0900

    ch code

--- Aw_copy.f90
+++ Aw_copy.f90
@@ -1,32 +1,4 @@
-program aaa
-IMPLICIT NONE
-integer i,j,jmax,jmin,numdata
-integer, parameter :: imax=10000,kmax=1000
-real :: t,x,y,dx,dy,sumi,sumr,om,z1,z2,u1,u2,F,xa,dxa,eta,omext,dom,x_dash,dx_dash,A,tau_a,gam,dt,dom_ext,ommax,ommin
-real, dimension(imax+1) :: xt,noise,noise_tilda
-
-
-!conditons
-parameter(ommax = 5.)
-parameter(numdata = 10000) !>> number of data point
-parameter(jmax=1+numdata)  !>> delta om_ext
-real, dimension(jmax+1) :: A_omext
-dom_ext=1.1/jmax
-
-
-!!!!!!!!!!!!!!!!!!!!!!!!노이즈 생성 !!!!!!!!!!!!!!!!!!
-call random_seed()  
-do i = 1, imax / 2
-call random_number(u1)
-call random_number(u2)
-z1 = sqrt(-2. * log(u1)) * cos(2.*3.14 * u2)
-z2 = sqrt(-2. * log(u1)) * sin(2.*3.14 * u2)
-noise(2 * i - 1) = z1
-noise(2 * i) = z2
-end do
-!!!!!!!!!!!!!!!!!!!!!노이즈 생성 끝!!!!!!!!!!!!!!!!!!
-
-
+!>> <Dimensionless process>
 !>> t0 == 1/om0
 !>> x_tlida == x/l 
 !>> x_tilda^dash == x^dash/l
@@ -35,14 +7,49 @@ end do
 !>> tau_a^tilda == om0 * tau_a
 
 
-!>> condations
+program aaa
+IMPLICIT NONE
+integer i,j,jmax,jmin,numdata,k
+integer, parameter :: imax=10000,kmax=1000
+real :: t,x,y,dx,dy,sumi,sumr,om,z1,z2,u1,u2,F,xa,dxa,eta,omext,dom,x_dash,dx_dash,A,tau_a,gam,dt,dom_ext,ommax,ommin
+real, dimension(imax+1) :: xt,noise,noise_tilda
+
+
+!>>conditons
+parameter(ommax = 5.)
+parameter(numdata = 10000) !>> number of data point
+parameter(jmax=1+numdata)  !>> delta om_ext
+real, dimension(jmax+1) :: A_omext
+dom_ext=1.1/jmax
+
+
+!>> condations22
 tau_a = 0.1
 eta = 1.
 gam = 0.14
 dt = 1.e-2
 
+!>> make noise
+call random_seed()  
+do i = 1, imax / 2
+call random_number(u1)
+call random_number(u2)
+z1 = sqrt(-2. * log(u1)) * cos(2.*3.14 * u2)
+z2 = sqrt(-2. * log(u1)) * sin(2.*3.14 * u2)
+noise(2 * i - 1) = z1
+noise(2 * i) = z2
+end do !>> end noise make
 
-do j = 1,jmax
+
+
+
+do k=1,10 !>> k is index of ext Force
+
+F=0.01*eta*k !>> grow ext Force
+
+
+do j = 1,jmax !>> j is index of om_ext
+
 !special conditon
 omext = j*dom_ext
 
@@ -52,8 +59,10 @@ x = 1.
 x_dash = 0.
 y = 0.
 
-F=0.01*eta
+
+
 do i=1,imax !>> i is time index
+
 xt(i) = x
 noise_tilda(i) = 1.e-2*noise(i)
 dy = -gam*y -x -noise_tilda(i) +(1./2.)*eta*sign(1.,x-x_dash) +F*sin(omext*t)
@@ -63,6 +72,7 @@ dx_dash = (x-x_dash)/tau_a
 y = y + dy*dt
 x = x + dx*dt
 x_dash = x_dash +dx_dash*dt 
+
 end do !>> i end // !>> fixed omext, recode xt
 
 A_omext(j) = 2.*sqrt(sumi**2 + sumr**2) !>> recode A_omext
@@ -71,17 +81,27 @@ sumr = 0.
 sumi = 0.
 t = 0.
 
-do i = 1,imax
+do i = 1,imax 
+
 t = i*dt
 sumi = sumi+xt(i)*sin(omext*t)*dt
 sumr = sumr+xt(i)*cos(omext*t)*dt
+
 end do !>> i end
+
+
 end do !>> j end >> fixed omext one special condition end
 
 
 open(2,file = 'Aw')  ! >> recode Aw_ext vs w_ext
-do j = 1,50
-omext = 0.1 * j
+
+do j = 1,jmax
+
+omext = j*dom_ext
 write(2,*) omext, A_omext(j) !  >> fixed ext Force.
-end do
+write(*,*) omext
+end do!>> j end
+write(2,*) '' !>> overlap the plot
+enddo!>> k end
+
 end program
