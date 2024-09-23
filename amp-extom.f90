25cf83dfa95568f316f1a2ee6ba052b41828d9bc
JiPang22 <wlvud6902@gmail.com>
2024-09-23 16:32:21 +0900

    aa

--- amp-extom.f90
+++ amp-extom.f90
@@ -1,15 +1,13 @@
 program aaa
 IMPLICIT NONE
 integer i, j, jmax, jmin, k
-!integer, parameter :: imax = 314159
-!integer, parameter :: imax = 628318
-integer, parameter :: imax = 1314159
+integer, parameter :: imax = int(1.e+5)
 real  t, x, y, dt, dx, dy, sumi, sumr, z1, z2, u1, u2, F, eta, om_ext
 real x_dash, dx_dash, tau_a, gam, dom_ext, om_max, om_min
 real, dimension(imax + 1) :: xt, noise, noise_tilda
 parameter(om_max = 1.1)
 parameter(om_min = 0.9)
-parameter(dt = 1.e-2)
+parameter(dt = 1.e-3)
 parameter(dom_ext = 6.28 / (imax * dt))
 parameter(jmin = int(om_min / dom_ext))
 parameter(jmax = int(om_max / dom_ext))
@@ -17,6 +15,7 @@ real, dimension(jmax+1) :: A_om_ext
 parameter(tau_a = 0.1)
 parameter(eta = 1.)
 parameter(gam = 0.2)
+open(1, file = 'xt') !>> make file!
 open(2, file = 'Aw') !>> make file!
 
 
@@ -32,8 +31,8 @@ noise(2 * i) = z2
 end do  !>>     end noise make
 
 
-do k = 1, 5 !>> k is index of ext Force
-F = 0.02 * eta * k       !>> grow ext Force magnitude
+do k = 1, 4 !>> k is index of ext Force
+F = 0.02 * eta * k        !>> grow ext Force magnitude
 
 do j = jmin, jmax !>> j is index of om_ext
 !special conditon
@@ -45,7 +44,8 @@ x_dash = 0.
 y = 0.
 do i = 1, imax !>> i is time index
 xt(i) = x  !>>> recode x(t)
-noise_tilda(i) = 1.e-3 * noise(i)   !>>  lowing noize magnitude
+write(1,*) t, x
+noise_tilda(i) =0.*1.e-3 * noise(i)   !>>  lowing noize magnitude
 dy = - gam * y - x + noise_tilda(i) + (1. / 2.) * eta * sign(1., x - x_dash) + F * sin(om_ext * t)
 dx = y
 dx_dash = (x - x_dash) / tau_a
@@ -53,15 +53,22 @@ y = y + dy * dt
 x = x + dx * dt
 x_dash = x_dash + dx_dash * dt 
 t = i * dt
-end do   !>> i end // !>> fixed om_ext, recode xt
+end do   !>> i end // !>> fixed om_ext, recode xt!
+write(1,*) ''
+
+
 sumr = 0.
 sumi = 0.
 t = 0.
+
+
 do i = 1, imax 
 t = i * dt
 sumi = sumi + xt(i) * sin(om_ext * t) * dt
 sumr = sumr + xt(i) * cos(om_ext * t) * dt
 end do  !>> i end
+
+
 A_om_ext(j) = 2. * sqrt(sumi**2 + sumr**2) !>> recode A_om_ext
 end do  !>> j end >> fixed om_ext one special condition end
 
@@ -69,8 +76,14 @@ end do  !>> j end >> fixed om_ext one special condition end
     !>> recoding amp - ext om
 do j = jmin, jmax
 om_ext = j * dom_ext
+
 write(2, *) om_ext, A_om_ext(j) !  >> fixed ext Force.
+
 end do  !>>   j end
 write(2, *) '' !>> overlap the plot
 end do  !>>   k end
+
+
+
+
 end program
